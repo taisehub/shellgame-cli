f0a43665c6fe3dd414289d71a8df9f019d08635b
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-07-31 22:49:36 +0900

    :sparkles: add container handler

---
+++ infrastructure/container.go
@@ -0,0 +1,88 @@
+package infrastructure
+
+import (
+	"context"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/client"
+	"log"
+	"net"
+	"time"
+)
+
+const (
+	IMAGE_NAME = "alphine"
+)
+
+var (
+	conf = &container.Config{
+		AttachStdin:  true,
+		AttachStdout: true,
+		AttachStderr: true,
+		Tty:          true,
+		Image:        IMAGE_NAME,
+	}
+	hconf = &container.HostConfig{
+		AutoRemove: true,
+	}
+	econf = types.ExecConfig{
+		AttachStdin:  true,
+		AttachStdout: true,
+		AttachStderr: true,
+		Tty:          true,
+		Cmd:          []string{"/bin/sh"},
+	}
+)
+
+type ContainerHandler struct {
+	client *client.Client
+}
+
+func NewContainerHandler() (*ContainerHandler, error) {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		return nil, err
+	}
+	h := new(ContainerHandler)
+	h.client = cli
+	return h, nil
+}
+
+func (h *ContainerHandler) Create(ctx context.Context, containerName string) (string, error) {
+	createdBody, err := h.client.ContainerCreate(ctx, conf, hconf, nil, nil, containerName)
+	if err != nil {
+		return "", err
+	}
+	if len(createdBody.Warnings) != 0 {
+		for warn := range createdBody.Warnings {
+			log.Printf("Warning in ContainerHandler.Create(): %v\n", warn)
+		}
+	}
+	return createdBody.ID, nil
+}
+
+func (h *ContainerHandler) Start(ctx context.Context, containerID string) error {
+	return h.client.ContainerStart(ctx, containerID, types.ContainerStartOptions{})
+}
+
+func (h *ContainerHandler) RunShell(ctx context.Context, containerName string) (net.Conn, error) {
+	iresp, err := h.client.ContainerExecCreate(ctx, containerName, econf)
+	if err != nil {
+		return nil, err
+	}
+	hresp, err := h.client.ContainerExecAttach(ctx, iresp.ID, types.ExecStartCheck{})
+	if err != nil {
+		return nil, err
+	}
+	return hresp.Conn, nil
+}
+
+func (h *ContainerHandler) Stop(ctx context.Context, containerID string) error {
+	timeout := time.Duration(3 * time.Second)
+	return h.client.ContainerStop(ctx, containerID, &timeout)
+}
+
+func (h *ContainerHandler) Remove(ctx context.Context, containerID string) error {
+	opt := types.ContainerRemoveOptions{RemoveVolumes: true, RemoveLinks: true, Force: false}
+	return h.client.ContainerRemove(ctx, containerID, opt)
+}
