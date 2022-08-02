9fea81d31d6f15e19a080a5d87aa7afe3bad9e8f
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-08-03 00:22:38 +0900

    :truck: move http router to main function

--- cmd/shellgame/main.go
+++ cmd/shellgame/main.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"log"
+	"net/http"
+)
+
 func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/hello", nil)
+	mux.Handle("/ws", nil)
+	
+	log.Println("[+] Start listening.")
+	http.ListenAndServe(":", mux)
 }
