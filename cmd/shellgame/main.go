03bca4a530cd8e2228b427005646ad136325d0f8
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-08-04 00:28:44 +0900

    :arrow_up: update main.go

--- cmd/shellgame/main.go
+++ cmd/shellgame/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"github.com/taise-hub/shellgame-cli/infrastructure"
 	"github.com/taise-hub/shellgame-cli/interfaces"
+	"github.com/taise-hub/shellgame-cli/usecase"
 	"log"
 	"net/http"
 )
 
 func main() {
-	gameController := interfaces.NewGameController()
+	containerHandler, err := infrastructure.NewContainerHandler()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	consoleRepo := interfaces.NewContainerRepository(containerHandler)
+	gameUsecase := usecase.NewGameInteractor(consoleRepo)
+	gameController := interfaces.NewGameController(gameUsecase)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", gameController.Hello)
 	mux.HandleFunc("/ws", gameController.HandleWebsocket)
