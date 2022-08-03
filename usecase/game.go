72c427c161d08c45b54fa013f1aa1e2bb35bba37
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-08-04 00:27:55 +0900

    :truck: rename usecase (not interface) to interactor

--- usecase/game.go
+++ usecase/game.go
@@ -1,23 +1,31 @@
 package usecase
 
-type GameUsecase struct {
+import (
+	"github.com/taise-hub/shellgame-cli/domain/repository"
+)
+
+type GameUsecase interface {
+	Start() error
+	Matching() error
 }
 
-func NewGameUsecase() *GameUsecase {
-	return &GameUsecase{}
+type gameInteractor struct {
+	shellRepo repository.ConsoleRepository
 }
 
-func (gc *GameUsecase) Hello() string {
-	return "Hello!!"
+func NewGameInteractor(shellRepo repository.ConsoleRepository) GameUsecase {
+	return &gameInteractor{
+		shellRepo: shellRepo,
+	}
 }
 
 // マッチング完了後、バトルを開始するためのメソッド
-func (gc *GameUsecase) Start() {
+func (gc *gameInteractor) Start() error {
 	panic("implement me")
 }
 
 // ゲームのマッチングをするためのメソッド
 // 詳細まで考えれていないので、最終的にはもう少し細かく分かれる
-func (gu *GameUsecase) Matching() {
+func (gu *gameInteractor) Matching() error {
 	panic("implement me")
 }
