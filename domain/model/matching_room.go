f284e9b6c4ed92d228d32ce6033445ef6b113844
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-08-06 16:42:48 +0900

    :sparkles: define the behavior of the waiting  for a match

---
+++ domain/model/matching_room.go
@@ -0,0 +1,48 @@
+package model
+
+var (
+	matchingRoom *MatchingRoom = &MatchingRoom{
+		MatchingPlayers: make(map[*MatchingPlayer]struct{}),
+		message:         make(chan *MatchingMessage),
+		register:        make(chan *MatchingPlayer),
+		unregister:      make(chan *MatchingPlayer),
+	}
+)
+
+// shellgame-cliサーバ上で一つだけ存在。
+// 対戦待ち状態の管理を行う。
+type MatchingRoom struct {
+	MatchingPlayers map[*MatchingPlayer]struct{} // 誰がMatchigRoomにいるのか把握するために利用。
+	message         chan *MatchingMessage
+	register        chan *MatchingPlayer
+	unregister      chan *MatchingPlayer
+}
+
+func GetMatchingRoom() *MatchingRoom {
+	return matchingRoom
+}
+
+func (mr *MatchingRoom) GetRegisterChan() (chan<- *MatchingPlayer) {
+	return mr.register
+}
+
+func (mr *MatchingRoom) GetUnregisterChan() (chan<- *MatchingPlayer) {
+	return mr.unregister
+}
+
+func (mr *MatchingRoom) Run() {
+	for {
+		select {
+		case matchingPlayer := <-mr.register:
+			mr.MatchingPlayers[matchingPlayer] = struct{}{}
+		case matchingPlayer := <-mr.unregister:
+			if _, ok := mr.MatchingPlayers[matchingPlayer]; ok {
+				//matchingPlayerが到達不可能オブジェクトになるはず(願望)
+				close(matchingPlayer.matchingChan)
+				delete(mr.MatchingPlayers, matchingPlayer) 
+			}
+		case message := <-mr.message:
+			message.dest.matchingChan <- message
+		}
+	}
+}
--- domain/model/player.go
+++ domain/model/player.go
@@ -1,17 +1,54 @@
 package model
 
-type Player struct {
-	profile Profile
-	state   State
+type Player interface {
+	GetProfile() *Profile
 }
 
-func NewPlayer(profile Profile) *Player {
-	return &Player{
-		profile: profile,
-		state:   INACTIVE,
+type MatchingPlayer struct {
+	profile      *Profile
+	state        State
+	conn         Conn
+	matchingChan chan *MatchingMessage
+}
+
+func NewMatchingPlayer(profile *Profile) *MatchingPlayer {
+	return &MatchingPlayer{
+		profile:      profile,
+		state:        INACTIVE,
+		matchingChan: make(chan *MatchingMessage),
 	}
 }
 
-func (p *Player) GetID() uint32 {
-	return p.profile.GetID()
+// FIXME: IDではなくProfileを返すようにする
+func (p *MatchingPlayer) GetProfile() *Profile {
+	return p.profile
+}
+
+// TODO: context対応
+func (p *MatchingPlayer) ReadPump() {
+	defer func() {
+		p.conn.Close()
+		GetMatchingRoom().unregister <- p
+	}()
+	var msg  *MatchingMessage
+	for {
+		if err := p.conn.Read(msg); err != nil {
+			return
+		}
+		GetMatchingRoom().message <- msg
+	}
+}
+
+// TODO: context対応
+func (p *MatchingPlayer) WritePump() {
+	defer p.conn.Close()
+	for {
+		msg, ok := <- p.matchingChan
+		if !ok {
+			return
+		}
+		if err := p.conn.Write(msg); err != nil {
+			return
+		}
+	}
 }
