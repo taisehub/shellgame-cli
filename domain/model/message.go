ab14f0cd8c579d33759173e1385fbe14fefdf63b
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-08-06 16:28:38 +0900

    :construction: design message model

---
+++ domain/model/message.go
@@ -0,0 +1,24 @@
+package model
+
+type Message interface {
+	GetSource() Player
+	GetDest() Player
+}
+
+type MatchingMessage struct {
+	source *MatchingPlayer
+	dest   *MatchingPlayer
+	data   *MatchingMessageData
+}
+
+func (mm *MatchingMessage) GetSource() Player{
+	return mm.source
+}
+
+func (mm *MatchingMessage) GetDest() Player{
+	return mm.dest
+}
+
+// TODO: マッチング時のメッセージのフォーマットの設計
+type MatchingMessageData struct {
+}
