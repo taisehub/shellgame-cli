12338e319c0fa37ce29a120bdc10f931e7ea8486
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-08-07 22:51:45 +0900

    :sparkles: add new endpoint

--- interfaces/game_controller.go
+++ interfaces/game_controller.go
@@ -1,16 +1,17 @@
 package interfaces
 
 import (
+	"encoding/json"
 	"fmt"
-	"time"
-	"math/rand"
-	"sync"
-	"github.com/gorilla/sessions"
 	"github.com/gorilla/securecookie"
+	"github.com/gorilla/sessions"
 	"github.com/gorilla/websocket"
-	"github.com/taise-hub/shellgame-cli/usecase"
 	"github.com/taise-hub/shellgame-cli/domain/model"
+	"github.com/taise-hub/shellgame-cli/usecase"
+	"math/rand"
 	"net/http"
+	"sync"
+	"time"
 )
 
 const (
@@ -72,13 +73,28 @@ func (con *GameController) saveProfile(w http.ResponseWriter, req *http.Request)
 	// もうちょっとスマートにidを生成をしたい
 	src := rand.NewSource(time.Now().UnixNano())
 	random := rand.New(src)
-  	sess.Values["id"] = random.Uint32()
+	sess.Values["id"] = random.Uint32()
 	// ------------
-  	sess.Values["name"] = name
-  	if err := store.Save(req, w, sess); err != nil {
-  	  http.Error(w, err.Error(), http.StatusInternalServerError)
-  	  return
-  	}
+	sess.Values["name"] = name
+	if err := store.Save(req, w, sess); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (con *GameController) Match(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case "GET":
+		con.getMatchPlayers(w, req)
+	default:
+		http.NotFound(w, req)
+	}
+}
+
+// MatchingPlayerのProfileを返すようにしたい。
+func (con *GameController) getMatchPlayers(w http.ResponseWriter, req *http.Request) {
+	players := con.usecase.GetMatchingPlayers()
+	RespondJSON(w, players, 200)
 }
 
 func (con *GameController) WaitMatch(w http.ResponseWriter, req *http.Request) {
@@ -90,4 +106,16 @@ func (con *GameController) WaitMatch(w http.ResponseWriter, req *http.Request) {
 	sess, _ := store.Get(req, SESS_NAME)
 	player := model.NewMatchingPlayer(sess.Values["id"].(uint32), sess.Values["name"].(string), &WebsocketConn{conn, sync.Mutex{}})
 	con.usecase.WaitMatch(player)
+}
+
+func RespondJSON(w http.ResponseWriter, body any, status int) {
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	bodyBytes, err := json.Marshal(body)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	w.WriteHeader(status)
+	if _, err := fmt.Fprintf(w, "%s", bodyBytes); err != nil {
+		fmt.Printf("write response error: %v", err)
+	}
 }
