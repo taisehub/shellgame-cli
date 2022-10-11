90d78365746e972ef284d87d8a60cc1fc90de51d
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-10-11 21:06:07 +0900

    :beers: Implement client struct

--- client/client.go
+++ client/client.go
@@ -2,12 +2,12 @@ package shellgame
 
 import (
 	"bytes"
+	"io/ioutil"
 	"encoding/json"
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 	"github.com/taise-hub/shellgame-cli/server/domain/model"
-	"log"
 	"net/http"
 	"net/url"
 	"time"
@@ -23,15 +23,28 @@ var (
 	shellEndpoint   = &url.URL{Scheme: "ws", Host: HOST, Path: "/shell"}
 )
 
-func newClient() (*http.Client, error) {
+// Clientはシェルゲーサーバとネットワーク的なやりとりを行う役割を持つ
+type Client struct {
+	*http.Client
+	baseEndpoint    *url.URL
+	profileEndpoint *url.URL
+	shellEndpoint 	*url.URL
+}
+
+func newClient() (*Client, error) {
+	c := &Client{}
 	jar, err := getJar()
 	if err != nil {
 		return nil, err
 	}
-	return &http.Client{
+	c.Client = &http.Client{
 		Jar:     jar,
 		Timeout: 20 * time.Second,
-	}, nil
+	}
+	c.baseEndpoint =  &url.URL{Scheme: "http", Host: HOST, Path: "/"}
+	c.profileEndpoint = &url.URL{Scheme: "http", Host: HOST, Path: "/profile"}
+	c.shellEndpoint   = &url.URL{Scheme: "ws", Host: HOST, Path: "/shell"}
+	return c, nil
 }
 
 // シェルゲーサーバで稼働するコンテナにWebSocketを利用して接続する。
@@ -44,6 +57,7 @@ func ConnectShell() (*websocket.Conn, error) {
 	for _, cookie := range jar.Cookies(baseEndpoint) {
 		header.Add("Cookie", fmt.Sprintf("%s=%s", cookie.Name, cookie.Value))
 	}
+
 	wsconn, _, err := websocket.DefaultDialer.Dial(shellEndpoint.String(), header)
 	if err != nil {
 		return nil, err
@@ -52,8 +66,8 @@ func ConnectShell() (*websocket.Conn, error) {
 	return wsconn, nil
 }
 
-// プロフィールをシェルゲーサーバに送信する。
-func PostProfile(name string) error {
+// シェルゲーサーバにプレイヤー名を登録する。
+func (c *Client) PostProfile(name string) error {
 	id := uuid.New()
 	profile := &model.Profile{ID: id.String(), Name: name}
 	p, err := json.Marshal(profile)
@@ -61,21 +75,25 @@ func PostProfile(name string) error {
 		return err
 	}
 
-	request, err := http.NewRequest("POST", profileEndpoint.String(), bytes.NewBuffer(p))
+	request, err := http.NewRequest("POST", c.profileEndpoint.String(), bytes.NewBuffer(p))
 	if err != nil {
 		return err
 	}
 	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
 
-	client, err := newClient()
-	if err != nil {
-		return err
-	}
-	resp, err := client.Do(request)
+	resp, err := c.Do(request)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	log.Println("response Status:", resp.Status)
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("failed to read response body")
+	}
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("%s", body)
+	}
+
 	return nil
 }
