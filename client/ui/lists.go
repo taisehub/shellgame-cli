d44fc2b56c83f5cd474add84da8fc08a47502358
井上大誠 <arinc0@inoueoomakotonoMacBook-Pro.local>
2022-10-14 09:32:45 +0900

    :recycle: separate ui parts by files

---
+++ client/ui/lists.go
@@ -0,0 +1,77 @@
+package ui
+
+import (
+	"fmt"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"io"
+)
+
+var (
+	screens = []list.Item{
+		screen("対戦"),
+		screen("終了"),
+		screen("ヘルプ"),
+	}
+)
+
+// TOP画面表示時の選択肢を扱うリスト
+type screen string
+
+func (i screen) FilterValue() string { return "" }
+
+type screenDelegate struct{}
+
+func (d screenDelegate) Height() int                               { return 1 }
+func (d screenDelegate) Spacing() int                              { return 0 }
+func (d screenDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+func (d screenDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(screen)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("* %s", i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s string) string {
+			return selectedItemStyle.Render(">  " + s)
+		}
+	}
+
+	fmt.Fprintf(w, fn(str))
+}
+
+
+// 対戦相手の選択肢を扱うリスト
+type Profile struct {
+	ID   string `json:"id"` 
+	Name string `json:"name"`
+}
+
+func (p Profile) FilterValue() string { return "" }
+
+type profileDelegate struct{}
+
+func (d profileDelegate) Height() int                               { return 1 }
+func (d profileDelegate) Spacing() int                              { return 0 }
+func (d profileDelegate) Update(msg tea.Msg, m *list.Model) tea.Cmd { return nil }
+func (d profileDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(Profile)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("* %s", i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s string) string {
+			return selectedItemStyle.Render(">  " + s)
+		}
+	}
+
+	fmt.Fprintf(w, fn(str))
+}
+
